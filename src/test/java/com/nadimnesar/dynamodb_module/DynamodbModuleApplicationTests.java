5b98247d630a075fc16f1b1ad8e86038189990b1
nadimnesar <nadimnesar.cse@gmail.com>
2025-02-10 15:59:34 +0600

    Initialize Springboot Project Setup

---
+++ src/test/java/com/nadimnesar/dynamodb_module/DynamodbModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadimnesar.dynamodb_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
