bbaa2fde2d1ac36bf64e28b8f1ce50a4596ea2b9
Nesar Ahmed <nadimnesar.cse@gmail.com>
2025-02-10 16:03:39 +0600

    Initialize Springboot Project Setup (#1)

---
+++ src/main/java/com/nadimnesar/dynamodb_module/DynamodbModuleApplication.java
@@ -0,0 +1,13 @@
+package com.nadimnesar.dynamodb_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbModuleApplication.class, args);
+	}
+
+}
